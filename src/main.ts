2bba824212aa10241fd9aa9530a59ff84d09e78f
GormanProg123 <gparfenchik2005@gmail.com>
2025-07-14 14:51:23 +0200

    [MAIN] feat: main change

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://vpn-frontend-ruddy.vercel.app',
+  ];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
+
   app.use(cookieParser());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

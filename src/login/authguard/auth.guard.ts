5db359e33e34d228705a585a3e51aab10c9e14a0
GormanProg123 <gparfenchik2005@gmail.com>
2025-08-13 17:36:55 +0200

    [VPN-23] feat: user protection

---
+++ src/login/authguard/auth.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private prisma: PrismaService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const token = req.cookies?.['auth_token'];
+    if (!token) throw new UnauthorizedException();
+
+    const session = await this.prisma.userSession.findUnique({
+      where: { token },
+      include: { user: true },
+    });
+
+    if (!session || !session.isActive || session.expiresAt < new Date()) {
+      throw new UnauthorizedException();
+    }
+
+    req.user = session.user;
+    return true;
+  }
+}
--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -7,12 +7,16 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
   Req,
 } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { CreateLoginDto, LoginConfigPath } from './dto/create-login.dto';
 import { ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
+import { AuthGuard } from 'src/login/authguard/auth.guard';
 import { UserIp } from 'src/clientIP/user-ip.decorator';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller('login')
 export class LoginController {
@@ -25,7 +29,7 @@ export class LoginController {
   })
   @Post('')
   async login(
-    @UserIp() ip:string,
+    @UserIp() ip: string,
     @Req() req,
     @Res({ passthrough: true }) res,
     @Body() createLoginDto: CreateLoginDto,
@@ -40,5 +44,27 @@ export class LoginController {
   @Post('out')
   logout(@Req() req, @Res({ passthrough: true }) res) {
     return this.loginService.logout(req, res);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('config')
+  async getConfig(@Req() req, @Res() res) {
+    const userId = req.user.id;
+
+    const filePath = path.join(
+      process.cwd(),
+      'configs',
+      `client-${userId}.conf`,
+    );
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).json({ message: 'Config not found' });
+    }
+
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename="client-${userId}.conf"`,
+    );
+    fs.createReadStream(filePath).pipe(res);
   }
 }
